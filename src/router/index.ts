9971a7996da520820f6acd58b27edcf04aef63d7
kashyap <kashyap@elitefit.ai>
2025-04-15 13:22:51 +0530

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AssessmentRoutes from './AssessmentRoutes';
 
 export const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
         AssessmentRoutes
     ],
